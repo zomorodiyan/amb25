5668a2ce8aed3aa4552255cb9ff97de7b5d88c06
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2025-08-18 14:07:25 -0700

    add / at the end of directories

--- smallFast/slurm_lbf.sh
+++ smallFast/slurm_lbf.sh
@@ -11,7 +11,7 @@
 #SBATCH --error=job_error.log
 
 # Define variables
-CASE_DIR=/home/mzomoro1/amb25/smallFast
+CASE_DIR=/home/mzomoro1/amb25/smallFast/
 SIF_PATH=/home/mzomoro1/amb25/lbf-he.sif
 LOG_TIME=$CASE_DIR/log.laserbeamFoam.time
 
